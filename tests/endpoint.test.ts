0fe5539fea05ce5032bbf961acce58b7d67b2b99
Jeremia <jeripane@gmail.com>
2024-07-01 20:33:18 +0700

    fix test script

--- tests/endpoint.test.ts
+++ tests/endpoint.test.ts
@@ -1,47 +1,29 @@
+interface Car {
+    id: number;
+    name: string;
+    harga: number;
+    foto: string;
+    start_rent: string;
+    finish_rent: string;
+    created_at: string;
+    updated_at: string;
+    created_by: string;
+    updated_by: string;
+}
+
 describe("Testing API endpoints", () => {
+    const expectedDataStructure: {
+        message: string;
+        resp: Car[];
+    } = {
+        message: expect.any(String),
+        resp: expect.arrayContaining([expect.any(Object) as Car]), // Use Car type
+    };
+
     it("should fetch data", async () => {
         const response = await fetch("http://localhost:8686/api/v1/cars");
         const data = await response.json();
-        expect(data).toEqual({
-            message: "Data cars ditemukan",
-            resp: [
-                {
-                    id: 2,
-                    name: "Innova",
-                    harga: 1500000,
-                    foto: "https://res.cloudinary.com/dcyojno0c/image/upload/v1717311051/Challenge_Binar/2022_Toyota_Kijang_Innova_2.4_G_GUN142R__2820220302_29_ery9sa.jpg",
-                    start_rent: "2024-06-01T17:00:00.000Z",
-                    finish_rent: "2024-06-01T17:00:00.000Z",
-                    created_at: "2024-06-02T08:28:04.611Z",
-                    updated_at: "2024-06-02T08:38:18.490Z",
-                    created_by: "Jepan",
-                    updated_by: "Heri",
-                },
-                {
-                    id: 7,
-                    name: "Fortuner",
-                    harga: 6500000,
-                    foto: "https://res.cloudinary.com/dcyojno0c/image/upload/v1717336661/Challenge_Binar/r7atcy1wjxi8wsgu59du.jpg",
-                    start_rent: "2024-06-01T17:00:00.000Z",
-                    finish_rent: "2024-06-01T17:00:00.000Z",
-                    created_at: "2024-06-02T13:57:41.627Z",
-                    updated_at: "2024-06-02T13:57:41.627Z",
-                    created_by: "Heri",
-                    updated_by: "Heri",
-                },
-                {
-                    id: 9,
-                    name: "alphard",
-                    harga: 7777777,
-                    foto: "https://res.cloudinary.com/dcyojno0c/image/upload/v1718974241/Challenge_5/ysuofta27cb55kr6kppx.jpg",
-                    start_rent: "2024-06-20T17:00:00.000Z",
-                    finish_rent: "2024-06-20T17:00:00.000Z",
-                    created_at: "2024-06-21T12:34:26.360Z",
-                    updated_at: "2024-06-21T12:50:40.039Z",
-                    created_by: "Jepan",
-                    updated_by: "Jepan",
-                },
-            ],
-        });
+
+        expect(data).toEqual(expect.objectContaining(expectedDataStructure));
     });
 });
